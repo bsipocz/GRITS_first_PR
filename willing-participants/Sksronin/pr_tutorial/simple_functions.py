198ba49ddd94597b3f615ab7a5a92e1211ae1ef7
Tracy Chen <starrytc@gmail.com>
2024-10-29 14:05:57 -0700

    added the is_prime function

--- willing-participants/Sksronin/pr_tutorial/simple_functions.py
+++ willing-participants/Sksronin/pr_tutorial/simple_functions.py
@@ -10,3 +10,17 @@ def factorial(value):
         return 1
     else:
         return value * factorial(value - 1)
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
